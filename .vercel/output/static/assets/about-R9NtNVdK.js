1169b87d8d27e8370da594d92d19451bc139614e
silambarasan <csesilambu3@gmail.com>
2026-06-03 16:57:08 +0530

    Remove generated Vercel output from repo

--- .vercel/output/static/assets/about-R9NtNVdK.js
+++
@@ -1,1 +0,0 @@
-import{e,r}from"./index-fz0Xxavz.js";function u(){const t=e();return r.useEffect(()=>{t.navigate({to:"/",replace:!0})},[t]),null}export{u as component};
--- .vercel/output/static/assets/contact-jVQL0byt.js
+++
@@ -1,1 +0,0 @@
-import{e,r}from"./index-fz0Xxavz.js";function n(){const t=e();return r.useEffect(()=>{t.navigate({to:"/",replace:!0})},[t]),null}export{n as component};
